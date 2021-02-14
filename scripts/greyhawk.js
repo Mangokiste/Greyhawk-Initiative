d8a7820ccd826ba56a6f4504693701c772de8190
Mangokiste <79049638+Mangokiste@users.noreply.github.com>
2021-02-14 18:06:35 +0100

    Update greyhawk.js

--- scripts/greyhawk.js
+++ scripts/greyhawk.js
@@ -2,17 +2,18 @@
 export class GreyhawkCombat extends Combat {
     @Override
     _sortCombatants(a, b) {
-    const ia = Number.isNumeric(a.initiative) ? a.initiative : 9999;
-    const ib = Number.isNumeric(b.initiative) ? b.initiative : 9999;
+      return super._sortCombatants(b, a);
+        /*const ia = Number.isNumeric(a.initiative) ? a.initiative : 9999;
+        const ib = Number.isNumeric(b.initiative) ? b.initiative : 9999;
 
-    let ci = ia - ib;
-    if ( ci !== 0 ) return ci;
-      
-    let [an, bn] = [a.token?.name || "", b.token?.name || ""];
-    let cn = an.localeCompare(bn);
-    if ( cn !== 0 ) return cn;
-                    
-    return a.tokenId - b.tokenId;
+        let ci = ia - ib;
+        if ( ci !== 0 ) return ci;
 
-  }
+        let [an, bn] = [a.token?.name || "", b.token?.name || ""];
+        let cn = an.localeCompare(bn);
+        if ( cn !== 0 ) return cn;
+
+        return a.tokenId - b.tokenId;
+        */
+      }
 }
