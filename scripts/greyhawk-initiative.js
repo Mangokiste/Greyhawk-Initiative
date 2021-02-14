23cae13678693fbe7ed20e0c71aaeb945770e595
Mangokiste <79049638+Mangokiste@users.noreply.github.com>
2021-02-14 18:34:19 +0100

    Update greyhawk-initiative.js

--- scripts/greyhawk-initiative.js
+++ scripts/greyhawk-initiative.js
@@ -3,7 +3,7 @@ import { GreyhawkCombat } from "./greyhawk-combat.js";
 function registerSettings() {
   console.log("greyhawk-initiative | Initializing Greyhawk Initiative Module");
   
-  CONFIG.Combat.entityClass = LancerCombat;
+  CONFIG.Combat.entityClass = GreyhawkCombat;
 }
 
 Hooks.once("init", registerSettings);
